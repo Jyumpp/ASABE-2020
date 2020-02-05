7f2b6d9530b94f5f939c781f2cb4d0bd05ed93aa
MichaelStarks <starksmichael224@gmail.com>
2020-02-05 15:59:55 -0500

    added light commenting to LineCorrection code

--- LineCorrection.py
+++ LineCorrection.py
@@ -4,11 +4,11 @@ import threading
 import time
 
 class LineCorrection:
-    test = lineTracking()
-    motor = []
+    test = lineTracking() ## linetrackign object
+    motor = [] ## list for motor
     d = dyn.DynamixelIO(device_name="/dev/ttyUSB0")
-    angle = None
-    error = 1
+    angle = None ##angle the robot is at relative to line
+    error = 1 ## amount of acceptable error
 
 
     def stop(self):
@@ -33,10 +33,11 @@ class LineCorrection:
         self.motor[2].set_velocity(-velocity)
         self.motor[3].set_velocity(-velocity)
 
-    def otherForward(self,velocity):
+    def backward(self,velocity):
         self.stop()
         self.forward(-velocity)
 
+    ## checks the angle measured by linetracing code
     def checkAngle(self):
         while True:
             self.angle =  self.test.getAngle()
@@ -62,6 +63,7 @@ class LineCorrection:
     def __init__(self):
         init = True
         while init:
+            ##initializes the motors
             try:
                 for motors in range(0,4):
                     self.motor.append(self.d.new_mx12_1(motors + 1))
@@ -71,12 +73,12 @@ class LineCorrection:
             except:
                 print("Plug in motors")
                 time.sleep(5)
-
+        ##starts a thread that checks the angle
         angleThread = threading.Thread(target=self.checkAngle)
         angleThread.setdaemon = True
         angleThread.start()
 
-
+        ##starts a thread to control mototion
         functionThread = threading.Thread(target=self.whatMove)
         functionThread.setdeamon = True
         functionThread.start()
