95cfa9514c692f64d2e42fa1fc1beaecd61c3df2
Michael Starks <starksmichael224@gmail.com>
2020-06-01 14:50:27 -0400

    Changed linecorrection to use translate

--- drive/Python/LineCorrection/LineCorrection.py
+++ drive/Python/LineCorrection/LineCorrection.py
@@ -1,50 +1,39 @@
-from LineTracing.lineTracing import lineTracing
 import multiprocessing as mp
 import time
-from Robot.Robot import *
 
 class LineCorrection:
-    angle = None
     error = .5
-    centerDistance = 0
-    halt = True
     robot = None
-
-    def checkAngle(self):
-        while True:
-            self.angle = self.pipeAngleR.recv()
-            self.centerDistance = self.pipeDistanceR.recv()
-
 
     def whatMove(self,robot):
         time.sleep(5)
         while True:
             try:
-                if self.angle is None or self.centerDistance is None:
-                    # print("None")
-                    continue
-                else:
-                    if self.centerDistance > .5:
-                        robot.drive(0)
-                        robot.translate(90,-self.centerDistance/2)
-                    elif self.centerDistance < -.5:
-                        robot.drive(0)
-                        robot.translate(90,abs(self.centerDistance)/2)
-                    print(self.angle)
-                    if self.angle < abs(self.error) and self.angle > -abs(self.error):
-                        robot.drive(512)
-                    elif self.angle > self.error:
-                        robot.drive(0)
-                        robot.centerAxis(-self.angle*.5)
-                    elif self.angle < -self.error:
-                        robot.drive(0)
-                        robot.centerAxis(abs(self.angle)*.5)
-                    time.sleep(.5)
+                #____________________________________Angle Correction______________________________#
+                print(self.angle.value)
+                if self.angle.value > self.error:
+                    robot.drive(0)
+                    robot.centerAxis(-self.angle.value*.5)
+                elif self.angle < -self.error:
+                    robot.drive(0)
+                    robot.centerAxis(abs(self.angle.value)*.5)
+                #___________________________________Offset Correction______________________________#
+                #Detirmines if correction is needed
+                if abs(self.distance.value) > .5:
+                    angle = math.atan(self.distance.value/2)
+                    distance = (self.distance.value**2 + 4) ** (1/2)
+                else self.distance < -.5:
+                    angle = 0
+                # Corrects path if needed otherwise continues forward
+                robot.dive(512) if (angle == 0 && distance == 0) else robot.translate(angle,distance)
+
+                time.sleep(.5)
+
             except Exception as e:
                 print(e)
                 robot.drive(0)
 
-
-    def __init__(self,commAR,commDR):
-        self.pipeAngleR = commAR
-        self.pipeDistanceR = commDR
+    def __init__(self,commAR,commDR,robot):
+        self.angle = commAR
+        self.distance = commDR
+        self.robot = robot
--- drive/Python/test.py
+++ drive/Python/test.py
@@ -3,13 +3,16 @@ from LineCorrection.LineCorrection import *
 import multiprocessing as mp
 
 if __name__ == '__main__':
+    #Creates Robot object
+    robot = Robot("/dev/ttyUSB0")
+
     #Creates program pipes
-    pipeAngleR,pipeAngleW = mp.Pipe()
-    pipeDistanceR,pipeDistanceW = mp.Pipe()
+    angleValue = mp.Value('d',0.0)
+    distanceValue = mp.Value('d',0.0)
 
     #Sets up lineTracing and LineCorrection classes
-    tracing = lineTracing(pipeAngleW,pipeDistanceW)
-    correction = LineCorrection(pipeAngleR,pipeDistanceR)
+    tracing = lineTracing(angleValue,distanceValue)
+    correction = LineCorrection(angleValue,distanceValue,robot)
 
     #Sets threading type to fork
     mp.set_start_method('fork')
@@ -17,9 +20,6 @@ if __name__ == '__main__':
     #Makes and starts lineTracing
     threadTrace = mp.Process(target=tracing.lineTracer, args=())
     threadTrace.start()
-
-    #Creates Robot object
-    robot = Robot("/dev/ttyUSB0")
 
     #Starts the thread to check robot angle (Python Threads append data to pipes)
     threadCheckPipe = mp.Process(target=c.checkAngle,args=())
